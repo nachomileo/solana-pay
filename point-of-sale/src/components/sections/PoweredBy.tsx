52d48013ae916bc7b6f77ccea950b7b6ecdc6d09
Nacho Mileo <ignacio.mileo@gmail.com>
2023-01-30 13:02:30 +0100

    Adding Salmon Logo

--- point-of-sale/src/components/sections/PoweredBy.tsx
+++ point-of-sale/src/components/sections/PoweredBy.tsx
@@ -1,11 +1,12 @@
 import React, { FC } from 'react';
 import { SolanaPayLogo } from '../images/SolanaPayLogo';
+import { SalmonLogo } from '../images/salmonlogo';
 import * as css from './PoweredBy.module.pcss';
 
 export const PoweredBy: FC = () => {
     return (
         <div className={css.root}>
-            Powered by <SolanaPayLogo />
+            Powered by <SolanaPayLogo /> & <SalmonLogo />
         </div>
     );
 };
