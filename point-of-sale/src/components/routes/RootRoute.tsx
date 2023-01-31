4ea113ef62b2cc3765b7259b406c568efaf9d08d
Nacho Mileo <ignacio.mileo@gmail.com>
2023-01-31 18:07:10 +0100

    Logo FIX

--- point-of-sale/src/components/routes/RootRoute.tsx
+++ point-of-sale/src/components/routes/RootRoute.tsx
@@ -75,8 +75,11 @@ export const RootRoute: FC = () => {
                 ) : (
                     <div className={css.logo}>
                         <SolanaPayLogo width={240} height={88} />
+                        <br /><br />
+                        <p>by</p>
                         <SalmonLogo width={240} height={88} />
                     </div>
+
                 )}
             </FullscreenProvider>
         </ThemeProvider>
